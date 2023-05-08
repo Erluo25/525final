148c773eb90fa67004c600086b357070c5e887c6
Erluo25 <erluo0225@gmail.com>
2023-05-07 23:17:19 -0400

    Final

--- plot.py
+++ plot.py
@@ -2,8 +2,21 @@ import torch
 import numpy as np
 import matplotlib.pyplot as plt
 
-a2c_x = torch.load('tx.pt').numpy()
-a2c_y = torch.load('ty.pt').numpy()
+
+
+# Only steer 2-1
+a2c_x_str2 = torch.load('tx_str2.pt').numpy()
+a2c_x_str3 = torch.load('tx_str3.pt').numpy()
+a2c_x_str3 = 20 + a2c_x_str3
+x = np.hstack((a2c_x_str2, a2c_x_str3))
+print(x[:40])
+print(x.shape)
+
+a2c_y_str2 = torch.load('ty_str2.pt').numpy()
+a2c_y_str3 = torch.load('ty_str3.pt').numpy()
+y = np.hstack((a2c_y_str2, a2c_y_str3))
+print(y.shape)
+
 
 def plot(x, y, title = "A2C", fn="./a2c.png", shown=False):
     plt.title(title)
@@ -14,4 +27,13 @@ def plot(x, y, title = "A2C", fn="./a2c.png", shown=False):
     plt.savefig(filename)
     plt.show()
 
-plot(a2c_x, a2c_y)
+plot(x, y, title="Only Steer", fn="./a2c_only_steer.png")
+
+#plot(a2c_x_str1, a2c_y_str1, title="A2C STAR", fn="./a2c_star.png")
+
+#plot(a2c_x_str2, a2c_y_str2, title="A2C STAR ONLY STEER 2-1", fn="./a2c_star_only_steer_2-1.png")
+
+#plot(a2c_x_str3, a2c_y_str3, title="A2C STAR ONLY STEER", fn="./a2c_star_only_steer.png")
+
+
+
